b27472173abc29be94739a0a28ceb8aa5d85bdfa
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-10-10 18:09:35 +0900

    Add: Functional test of post module.

---
+++ test/phpunit/functional/frontend/postActionsTest.php
@@ -0,0 +1,29 @@
+<?php
+require_once dirname(__FILE__).'/../../bootstrap/functional.php';
+
+class functional_frontend_postActionsTest extends sfPHPUnitBaseFunctionalTestCase
+{
+  protected function getApplication()
+  {
+    return 'frontend';
+  }
+
+  public function testDefault()
+  {
+    $browser = $this->getBrowser();
+
+    $browser->
+      get('/post/index')->
+
+      with('request')->begin()->
+        isParameter('module', 'post')->
+        isParameter('action', 'index')->
+      end()->
+
+      with('response')->begin()->
+        isStatusCode(200)->
+        checkElement('body', '!/This is a temporary page/')->
+      end()
+    ;
+  }
+}
