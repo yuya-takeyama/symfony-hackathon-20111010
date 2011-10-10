5e3dc1d5c49e57d86edc9e983c5ef745cc2f97d5
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-10-10 18:12:38 +0900

    Fix: File format.

--- test/phpunit/functional/frontend/postActionsTest.php
+++ test/phpunit/functional/frontend/postActionsTest.php
@@ -3,27 +3,27 @@ require_once dirname(__FILE__).'/../../bootstrap/functional.php';
 
 class functional_frontend_postActionsTest extends sfPHPUnitBaseFunctionalTestCase
 {
-  protected function getApplication()
-  {
-    return 'frontend';
-  }
+    protected function getApplication()
+    {
+        return 'frontend';
+    }
 
-  public function testDefault()
-  {
-    $browser = $this->getBrowser();
+    public function testDefault()
+    {
+        $browser = $this->getBrowser();
 
-    $browser->
-      get('/post/index')->
+        $browser->
+            get('/post/index')->
 
-      with('request')->begin()->
-        isParameter('module', 'post')->
-        isParameter('action', 'index')->
-      end()->
+            with('request')->begin()->
+                isParameter('module', 'post')->
+                isParameter('action', 'index')->
+            end()->
 
-      with('response')->begin()->
-        isStatusCode(200)->
-        checkElement('body', '/Blog posts/')->
-      end()
-    ;
-  }
+            with('response')->begin()->
+                isStatusCode(200)->
+                checkElement('body', '/Blog posts/')->
+            end()
+        ;
+    }
 }
