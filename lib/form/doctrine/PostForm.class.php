372f8fc12b47c8a18c4644110212163ccd74ac0a
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-10-10 14:45:03 +0900

    Improve: Added validations.

--- lib/form/doctrine/PostForm.class.php
+++ lib/form/doctrine/PostForm.class.php
@@ -13,5 +13,16 @@ class PostForm extends BasePostForm
     public function configure()
     {
         $this->useFields(array('id', 'title', 'body'));
+
+        $ws = $this->getWidgetSchema();
+        $ws['title']->setAttribute('maxlength', 50);
+        $ws['body']->setAttribute('cols', 80)
+                   ->setAttribute('rows', 8);
+
+        $vs = $this->getValidatorSchema();
+        $vs['title']->setOption('required', true)
+                    ->setMessage('required', '未入力です');
+        $vs['body']->setOption('required', true)
+                   ->setMessage('required', '未入力です');
     }
 }
